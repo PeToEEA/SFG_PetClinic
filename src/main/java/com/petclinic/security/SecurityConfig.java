0b3960eaf11606848916959eefd5b39c05871a94
PeToEEA <peter.toman@eea.sk>
2019-01-23 14:09:36 +0100

    Initial commit...

---
+++ src/main/java/com/petclinic/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.petclinic.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/css/**", "/", "/index").permitAll()
+								.antMatchers("/member/**").hasRole("USER")
+								.and()
+								.formLogin().loginPage("/login").failureUrl("/login-error");
+		
+		
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("John")
+			.password("1234")
+			.roles("USER");
+									
+	}
+	
+	/*
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user = userDetailsService = 
+				User.withDefaultPasswordEncoder()
+					.username("John")
+					.password("1234")
+					.roles("USER")
+					.build();
+		
+		return new InMemoryUserDetailsManager(user);
+	}
+	*/
+	
+}
