469b766c56f62f2e87306f4aaed5031d53fe68a9
PeToEEA <peter.toman@eea.sk>
2019-01-31 08:34:40 +0100

    fixed errors

--- src/main/java/com/petclinic/repositories/MemberRepository.java
+++ src/main/java/com/petclinic/repositories/MemberRepository.java
@@ -1,7 +1,7 @@
 package com.petclinic.repositories;
 
 import com.petclinic.model.Member;
-import org.springframework.data.repository.*;
+import org.springframework.data.repository.CrudRepository;
 
 public interface MemberRepository extends CrudRepository<Member, Long> {
 
--- src/main/java/com/petclinic/repositories/OwnerRepository.java
+++ src/main/java/com/petclinic/repositories/OwnerRepository.java
@@ -2,6 +2,8 @@ package com.petclinic.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<OwnerRepository, Long> {
+import com.petclinic.model.Owner;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
 }
