9d57e7b5ec3239070af65c8e83492760b9ba5b50
PeToEEA <peter.toman@eea.sk>
2019-02-01 11:54:04 +0100

    removed unnecessary data repository

--- src/main/java/com/petclinic/repositories/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.petclinic.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.petclinic.model.Owner;
-
-public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
-}
